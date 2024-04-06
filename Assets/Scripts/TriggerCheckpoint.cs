5ad8c458a1dcfd7a7458c3cfdb5e29e9ae585090
AntoineTheOne <Antoine.Denis2@outlook.com>
2024-04-06 12:45:50 -0400

    ajout de plusieur truc

--- Assets/Scripts/TriggerCheckpoint.cs
+++ Assets/Scripts/TriggerCheckpoint.cs
@@ -2,22 +2,14 @@ using UnityEngine;
 
 public class TriggerCheckpoint : MonoBehaviour
 {
+    [SerializeField] private GameObject zoneTouche;
 
-
-[SerializeField] private GameObject zoneTrigger;
-
-public float score = 0;
-
-private void OnTriggerEnter(Collider other) {
-    
-    if(other.CompareTag("Player")){
-        zoneTrigger.SetActive(false);
-        score++;
-        Debug.Log(score);
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("Tout Fonctionne ici!!!");
+    if (other.tag == "Player")
+    {
+        zoneTouche.SetActive(false);
+        Debug.Log("Tout Fonctionne ici!!!");
     }
-
 }
-
-
-
 }
